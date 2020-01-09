1a6deea354bd59c8baf1278772e7380bbc6953ca
yangjin <13246856469@163.com>
2020-01-09 22:00:14 +0800

    up

--- 044/main.go
+++ 044/main.go
@@ -1,11 +1,13 @@
 package main
 
+import "fmt"
+
 func isMatch(s string, p string) bool {
 	i := 0
 	j := 0
 
-	strStarIdx := 0 // string中匹配'*'的位置
-	patStarIdx := -1    // pattern中出现'*'的位置
+	strStarIdx := 0  // string中匹配'*'的位置
+	patStarIdx := -1 // pattern中出现'*'的位置
 	for i < len(s) {
 		if j < len(p) && (s[i] == p[j] || p[j] == '?') {
 			i++
@@ -21,8 +23,8 @@ func isMatch(s string, p string) bool {
 			// 如果str和pattern的当前字符没有匹配上，并且pattern中的前一个字符是'*'，
 			// 那就让'*'尽可能多的匹配，都认为是和'*'匹配和(直到在str中找到匹配的字符)
 
-			j = patStarIdx + 1 // j指向pattern中'*'后面的那个字符，每次不匹配后，都从'*'后的字符重新匹配
-			strStarIdx += 1 // 如果str不匹配pattern中'*'后面的那个字符，则让str往后走，都认为是和'*'匹配
+			j = patStarIdx + 1 // j指向pattern中'*'后面的那个字符，每次不匹配后，都从'*'后的字符重新匹配 // 很关键，这里就是回溯！！！
+			strStarIdx += 1    // 如果str不匹配pattern中'*'后面的那个字符，则让str往后走，都认为是和'*'匹配
 			i = strStarIdx
 		} else {
 			return false
@@ -38,3 +40,8 @@ func isMatch(s string, p string) bool {
 	return true
 }
 
+func main() {
+	s := "cdddadab"
+	p := "*ab"
+	fmt.Println(isMatch(s, p))
+}
